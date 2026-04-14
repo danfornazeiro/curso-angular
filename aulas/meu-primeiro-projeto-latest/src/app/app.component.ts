8e7c047ddf7ffe8dc52d7e0a66b348cdf966417e
Nelson Aparecido Fornazeiro <nelson.fornazeiro@avanade.com>
2026-04-14 18:16:41 -0300

    secao 7

--- aulas/meu-primeiro-projeto-latest/src/app/app.component.ts
+++ aulas/meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { NewComponentComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from './components/template-variables/template-variables.component';
+import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
 
 @Component({
   selector: 'app-root',
@@ -13,13 +14,17 @@ import { TemplateVariablesComponent } from './components/template-variables/temp
     RouterOutlet,
     NewComponentComponent,
     TemplateBindingComponent,
-    TemplateVariablesComponent
+    TemplateVariablesComponent,
+    TemplateControlFlowComponent
   ],
   template: `
-  <h1>Meu Primeiro Projeto Angular</h1>
+  <!--<h1>Meu Primeiro Projeto Angular</h1>
 
   <app-template-binding />
-  <app-template-variables />
+  <app-template-variables />-->
+  <h1>Curso de Angular</h1>
+
+  <app-template-control-flow />
   `
 })
 export class AppComponent {
---
+++ aulas/meu-primeiro-projeto-latest/src/app/components/template/template-control-flow/template-control-flow.component.ts
@@ -0,0 +1,30 @@
+import { AsyncPipe, NgFor, NgIf, NgSwitch } from '@angular/common';
+import { Component } from '@angular/core';
+import { delay, Observable, of } from 'rxjs';
+
+@Component({
+  selector: 'app-template-control-flow',
+  imports: [AsyncPipe, NgFor, NgIf, NgSwitch],
+  templateUrl: './template-control-flow.component.html',
+  styleUrl: './template-control-flow.component.css'
+})
+export class TemplateControlFlowComponent {
+  public isTrue: boolean = true;
+  public itens = [{ name: 'Nelson Fornazeiro'}];
+  public itensNew: Array<{ name: string }> = [];
+  public switchValue = "A";
+  public loadingData$: Observable<string[]> = of([
+    'Data 1',
+    'Data 2',
+    'Data 3'
+  ]).pipe(delay(3000));
+
+  public trackByFn(index: number) {
+    return index;
+  }
+
+  public addNewName(value: string) {
+    this.itens.push({ name: value });
+  }
+
+}
